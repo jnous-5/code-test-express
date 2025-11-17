931434af95f77e22ef38be374aa3d34c6ff52f3f
Jhoncel Nayve <jhoncelnayve@gmail.com>
2025-11-17 11:40:57 +0000

    Run formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 
 import eslint from '@eslint/js';
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import perfectionist from 'eslint-plugin-perfectionist'
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import perfectionist from 'eslint-plugin-perfectionist';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  perfectionist.configs['recommended-natural'],
-  eslintConfigPrettier,
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    perfectionist.configs['recommended-natural'],
+    eslintConfigPrettier,
 );
